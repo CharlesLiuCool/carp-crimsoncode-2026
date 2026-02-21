9e32b3283e6aae317c7b5b80cf5b6f7c829bca42
AnhDucDuong <anh.duong@wsu.edu>
2026-02-21 13:48:29 -0800

    model

--- hospital_client/upload_weights.py
+++ hospital_client/upload_weights.py
@@ -3,7 +3,7 @@ import requests
 url = "http://127.0.0.1:8000/upload"
 
 files = {
-    "file": open("hospital_client/private_weights.pt", "rb")
+    "file": open("model/private_weights.pt", "rb")
 }
 
 response = requests.post(url, files=files)
